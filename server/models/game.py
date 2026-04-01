4204c8f29123e9c041d5d37d1a9bf380c1283d68
Deividi Santos <deividi.santos0724@jala.university>
2026-04-01 17:55:36 -0300

    feat: add game repository for MongoDB persistence

--- server/models/game.py
+++ server/models/game.py
@@ -3,9 +3,20 @@ from typing import List
 
 @dataclass
 class Game:
-    secret_word: str
-    player_1: str
-    player_2: str
-    guessed_letters: List[str] = field(default_factory=list)
-    errors: int = 0
-    status: str = "IN_PROGRESS" # Can be: IN_PROGRESS, VICTORY, DEFEAT
+    def __init__(self, secret_word, player_1):
+        self.secret_word = secret_word
+
+        self.owner = player_1
+        self.players = [player_1]  
+        
+        self.current_turn = 0
+
+        self.guessed_letters = []
+        self.player_errors = {
+            player_1: 0
+        }
+        self.max_errors = 6
+        self.player_status = {
+            player_1: "ALIVE"
+        }
+        self.status = "WAITING"  # WAITING | IN_PROGRESS | VICTORY | DEFEAT
