220f22b8ef367580292ebfc07a4c5e1c0f5d12d4
Deividi Santos <deividi.santos0724@jala.university>
2026-04-21 09:15:01 -0300

    fix: allow werkzeug

--- app.py
+++ app.py
@@ -267,4 +267,4 @@ def winner_page():
 # =========================
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
